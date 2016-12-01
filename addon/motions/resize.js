aaf0c3e9c21990fb1037c7fce47e212709e90c89
Edward Faulkner <edward@eaf4.com>
2016-12-01 16:06:32 -0500

    velocity-free version of resize

--- addon/motions/resize.js
+++ addon/motions/resize.js
@@ -1,16 +1,23 @@
-import velocity from 'dummy/velocity';
 import Motion from '../motion';
+import Tween from '../tween';
 import { task } from 'ember-concurrency';
+import { rAF } from '../concurrency-helpers';
 
 export default Motion.extend({
   animate: task(function *() {
+    let element = this.sprite.element;
     let duration = this.opts.duration;
     if (this.opts.duration == null) {
       duration = 500;
     }
-    yield velocity(this.sprite.element, {
-      width: [this.sprite.finalBounds.width, this.sprite.initialBounds.width],
-      height: [this.sprite.finalBounds.height, this.sprite.initialBounds.height]
-    }, { duration });
+
+    let width = new Tween(this.sprite.initialBounds.width, this.sprite.finalBounds.width, duration);
+    let height = new Tween(this.sprite.initialBounds.height, this.sprite.finalBounds.height, duration);
+
+    while (!width.done) {
+      yield rAF();
+      element.style.width = width.currentValue + 'px';
+      element.style.height = height.currentValue + 'px';
+    }
   })
 });
