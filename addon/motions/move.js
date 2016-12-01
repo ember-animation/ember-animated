098d7a0cf25c529927d17f264b8f9392ca3715fa
Edward Faulkner <edward@eaf4.com>
2016-12-01 13:45:32 -0500

    example of a velocity-free move

--- addon/motions/move.js
+++ addon/motions/move.js
@@ -1,27 +1,37 @@
-import velocity from 'dummy/velocity';
 import Motion from '../motion';
 import { task } from 'ember-concurrency';
+import { rAF } from '../concurrency-helpers';
+
+function ease(p) {
+  return 0.5 - Math.cos( p * Math.PI ) / 2;
+}
 
 export default Motion.extend({
   animate: task(function *() {
-    let tx = this.sprite.transform.tx;
-    let ty = this.sprite.transform.ty;
+    // Taking a queue from velocity, we don't directly use the rAF
+    // high resolution timestamps because of the way they get off
+    // under stress.
+    let startTime = (new Date()).getTime();
+    let duration = this.opts.duration;
+    if (duration == null) {
+      duration = 1000;
+    }
+    let runTime = 0;
+
+    let initialX = this.sprite.transform.tx;
+    let initialY = this.sprite.transform.ty;
     let dx = this.sprite.finalBounds.left - this.sprite.initialBounds.left;
     let dy = this.sprite.finalBounds.top - this.sprite.initialBounds.top;
 
-    //-------------------------------------------------------|
-    // This is a workaround for https://github.com/julianshapiro/velocity/issues/543
-    velocity.hook(this.sprite.element, 'translateX', tx); //|
-    velocity.hook(this.sprite.element, 'translateY', ty);   //|
-    // ------------------------------------------------------
-
-    let duration = this.opts.duration;
-    if (duration == null) {
-      duration = 500;
+    while (runTime < duration) {
+      this.sprite.reveal();
+      yield rAF();
+      runTime = (new Date()).getTime() - startTime;
+      let fraction = runTime / duration;
+      let eased = ease(fraction);
+      let newX = initialX + dx * eased;
+      let newY = initialY + dy * eased;
+      this.sprite.translate(newX - this.sprite.transform.tx, newY - this.sprite.transform.ty);
     }
-    yield velocity(this.sprite.element, {
-      translateX: [tx+dx, tx],
-      translateY: [ty+dy, ty]
-    }, { duration, begin: () => this.sprite.reveal() });
   })
 });
