43b54f2a0a3cdb651b20700ca681ec59919a8a0b
Edward Faulkner <edward@eaf4.com>
2016-12-02 09:15:14 -0500

    smooth interruption for Resize

--- addon/motions/resize.js
+++ addon/motions/resize.js
@@ -4,20 +4,46 @@ import { task } from 'ember-concurrency';
 import { rAF } from '../concurrency-helpers';
 
 export default Motion.extend({
+  init() {
+    this._super(...arguments);
+    this.prior = null;
+    this.widthTween = null;
+    this.heightTween = null;
+  },
+
+  interrupt: task(function * (motions) {
+    this.prior = motions.find(m => m instanceof this.constructor);
+    motions.forEach(motion => motion.cancel());
+  }),
+
   animate: task(function *() {
-    let element = this.sprite.element;
+    let sprite = this.sprite;
+    let element = sprite.element;
     let duration = this.opts.duration;
     if (this.opts.duration == null) {
-      duration = 500;
+      duration = 2000;
     }
 
-    let width = new Tween(this.sprite.initialBounds.width, this.sprite.finalBounds.width, duration);
-    let height = new Tween(this.sprite.initialBounds.height, this.sprite.finalBounds.height, duration);
+    if (!this.prior) {
+      this.widthTween = new Tween(sprite.initialBounds.width, sprite.finalBounds.width, duration);
+      this.heightTween = new Tween(sprite.initialBounds.height, sprite.finalBounds.height, duration);
+    } else {
+      this.widthTween = new Tween(
+        0,
+        sprite.finalBounds.width - this.prior.sprite.finalBounds.width,
+        duration
+      ).plus(this.prior.widthTween);
+      this.heightTween = new Tween(
+        0,
+        sprite.finalBounds.height - this.prior.sprite.finalBounds.height,
+        duration
+      ).plus(this.prior.heightTween);
+    }
 
-    while (!width.done) {
+    while (!this.widthTween.done) {
       yield rAF();
-      element.style.width = width.currentValue + 'px';
-      element.style.height = height.currentValue + 'px';
+      element.style.width = this.widthTween.currentValue + 'px';
+      element.style.height = this.heightTween.currentValue + 'px';
     }
   })
 });
