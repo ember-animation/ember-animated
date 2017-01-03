fa11122dde2a6f4c9cb247a4776afa9769918de2
Edward Faulkner <ef@alum.mit.edu>
2017-01-03 15:58:46 -0500

    adding a possible future test

--- tests/integration/components/animated-each-test.js
+++ tests/integration/components/animated-each-test.js
@@ -111,5 +111,39 @@ test('it updates when list is mutated', function(assert) {
   });
 });
 
+// I am unsure whether we care about supporting this behavior. As of
+// right now, it can do surprising things: (1) we don't observe the
+// change, (2) if you somehow trigger rerender anyway, you can end up
+// with duplicate list entries. To avoid this we would need to do more
+// preemptive copying.
+skip('it animates when an id is mutated', function(assert) {
+  let transitionCounter = 0;
+  this.set('items', A([{ id: 'a'}, {id: 'b'}, {id: 'c'}]));
+  this.set('transition', function * () {
+    if (++transitionCounter === 2) {
+      assert.equal(this.keptSprites.length, 2, 'kept sprites');
+      assert.equal(this.insertedSprites.length, 1, 'inserted sprites');
+      assert.equal(this.removedSprites.length, 1, 'removed sprites');
+    }
+  });
+  this.render(hbs`
+    {{#animated-each items use=transition key="id" as |item|}}
+      <div class="test-child">{{item.id}}</div>
+    {{/animated-each}}
+  `);
+
+  return this.waitForAnimations().then(() => {
+    Ember.set(this.get('items')[1], 'id', 'x');
+
+    // You can uncomment this to trigger the rerender behavior.
+    // this.set('items', this.get('items').slice());
+
+    return this.waitForAnimations();
+  }).then(() => {
+    assert.listContents(this.$('.test-child'), ['a', 'x', 'c']);
+    assert.equal(transitionCounter, 2, 'transitionCounter');
+  });
+});
+
 
 skip('check (on several platforms) whether we really need the ember-animated-hidden class. It may not be necessary with our microtask-based motions');
