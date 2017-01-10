9983b2b7bc02d909bb726d6cbe07d6c0c0b9b492
Edward Faulkner <ef@alum.mit.edu>
2017-01-10 12:45:55 -0500

    revised task management

---
+++ addon/scheduler.js
@@ -0,0 +1,182 @@
+
+/*
+
+API
+===
+
+  spawn: GenFn -> CancelablePromise
+
+    starts a new microroutine, which will run independently until it
+    finishes or is canceled.
+
+  spawnLinked: GenFn -> CancelablePromise
+
+    starts a new microroutine linked to the current
+    microroutine. Throws if your'e not in a current
+    microroutine.
+
+    "Linked" means that if the caller is canceled, the new
+    microroutine is also canceled. The inverse is not true: if the new
+    microroutine is canceled, the caller is not canceled.
+
+    `cancel(caller)` causes children to be canceled regardless of what
+    state caller is in. For example, even if caller has finished, any
+    children that haven't already finished will still be canceled.
+
+  stop: CancelablePromise -> null
+
+    cancel the microroutine represented by this promise.
+
+  current: () -> CancelablePromise
+
+    return the promise representing the currently executing
+    microroutine, or null if there is none. This exists because
+    spawning happens synchronously, meaning it begins running before
+    spawn returns, meaning there's no other way to get an early handle
+    on yourself.
+
+
+*/
+
+import { Promise } from './concurrency-helpers';
+
+export function spawn(genFn) {
+  let m = new MicroRoutine(genFn, false);
+  return m.promise;
+}
+
+export function spawnLinked(genFn) {
+  let m = new MicroRoutine(genFn, true);
+  return m.promise;
+}
+
+export function stop(microRoutinePromise) {
+  if (microRoutinePromise === current()) {
+    let e = new Error('TaskCancelation');
+    e.message = 'TaskCancelation';
+    throw e;
+  }
+  let microRoutine = microRoutines.get(microRoutinePromise);
+  if (microRoutine) {
+    microRoutine.stop();
+  }
+}
+
+export function logErrors(fn) {
+  ensureCurrent('logErrors').errorLogger = fn;
+}
+
+export function current() {
+  let current = getCurrent();
+  if (current) {
+    return current.promise;
+  }
+}
+
+let withCurrent, getCurrent;
+{
+  let current;
+  withCurrent = function(routine, fn) {
+    let prior = current;
+    current = routine;
+    try {
+      return fn();
+    } finally {
+      current = prior;
+    }
+  };
+  getCurrent = function() {
+    return current;
+  };
+}
+
+function ensureCurrent(label) {
+  let current = getCurrent();
+  if (!current) {
+    throw new Error(`${label}: only works inside a running microroutine`);
+  }
+  return current;
+}
+
+
+let loggedErrors = new WeakMap();
+let microRoutines = new WeakMap();
+
+class MicroRoutine {
+  constructor(genFn, linked) {
+    this.linked = [];
+    this.generator = genFn();
+    this.promise = new Promise((res, rej) => {
+      this.resolve = res;
+      this.reject = rej;
+    });
+    microRoutines.set(this.promise, this);
+    this.promise.__ec_cancel__ = this.stop.bind(this);
+    if (linked) {
+      let parent = ensureCurrent('spawnLinked');
+      parent.linked.push(this);
+      this.errorLogger = parent.errorLogger;
+    } else {
+      this.errorLogger = null;
+    }
+    this.wake('fulfilled', undefined);
+  }
+  wake(state, value) {
+    try {
+      if (state === 'fulfilled') {
+        this.state = withCurrent(this, () => this.generator.next(value));
+      } else {
+        this.state = withCurrent(this, () => this.generator.throw(value));
+      }
+      if (this.state.done) {
+        this.resolve(this.state.value);
+      } else {
+        Promise.resolve(this.state.value).then(
+          value => this.wake('fulfilled', value),
+          reason => this.wake('rejected', reason)
+        );
+      }
+    } catch(err) {
+      this.state = {
+        done: true
+      };
+      this.linked.forEach(microRoutine => {
+        microRoutine.stop();
+      });
+      if (err.message !== 'TaskCancelation') {
+        this.reject(err);
+        if (this.errorLogger) {
+          if (!loggedErrors.get(err)) {
+            loggedErrors.set(err, true);
+            this.errorLogger.call(null, err)
+          }
+        }
+      }
+    }
+  }
+  stop() {
+    if (isPromise(this.state.value) && typeof this.state.value.__ec_cancel__ === 'function') {
+      this.state.value.__ec_cancel__();
+    }
+    cancelGenerator(this.generator);
+    this.linked.forEach(microRoutine => {
+      microRoutine.stop();
+    });
+  }
+}
+
+function cancelGenerator(generator) {
+  let e = new Error('TaskCancelation');
+  e.message = 'TaskCancelation';
+  try {
+    generator.throw(e);
+  } catch(err) {
+    if (err.message !== 'TaskCancelation') {
+      throw err;
+    }
+  }
+}
+
+function isPromise(thing) {
+  return thing && typeof thing.then === 'function';
+}
---
+++ tests/unit/scheduler-test.js
@@ -0,0 +1,431 @@
+import { module, test } from 'qunit';
+import { installLogging } from '../helpers/assertions';
+import {
+  spawn,
+  spawnLinked,
+  logErrors,
+  stop,
+  current
+} from 'ember-animated/scheduler';
+import { Promise, microwait } from 'ember-animated/concurrency-helpers';
+
+module("Unit | scheduler", {
+  beforeEach(assert) {
+    installLogging(assert);
+  }
+});
+
+test('spawn starts synchronously', function(assert) {
+  let p = spawn(function * () {
+    assert.log("hello world");
+  });
+  assert.logEquals(['hello world']);
+  return p;
+});
+
+test('spawn continues asynchronously', function(assert) {
+  let p = spawn(function * () {
+    assert.log("hello world");
+    yield microwait();
+    assert.log("goodbye");
+  });
+  assert.logEquals(['hello world']);
+  return p.then(() => {
+    assert.logEquals(['hello world', 'goodbye']);
+  });
+});
+
+test('the return value of the spawned task is resolved', function(assert) {
+  return spawn(function * () {
+    return 42;
+  }).then(value => {
+    assert.equal(value, 42);
+  });
+});
+
+test('spawn synchronous exception', function(assert) {
+  return spawn(function * () {
+    throw new Error('boom');
+  }).then(() => {
+    assert.ok(false, "should not get here");
+  }, err => {
+    assert.equal(err.message, 'boom');
+  });
+});
+
+test('spawn: asynchronous exception', function(assert) {
+  let resolve;
+  let p = spawn(function * () {
+    yield new Promise(r => resolve = r);
+    throw new Error('boom');
+  });
+  resolve();
+  return p.then(() => {
+    assert.ok(false, "should not get here");
+  }, err => {
+    assert.equal(err.message, 'boom');
+  });
+});
+
+test('spawn within spawn', function(assert) {
+  let resolve;
+  let p = spawn(function * () {
+    assert.log("parent started");
+    let child = spawn(function * () {
+      assert.log("child started");
+      yield new Promise(r => resolve = r);
+      assert.log("child finishing");
+    });
+    assert.log("parent finishing");
+    yield child;
+    return 42;
+  })
+  assert.logEquals(["parent started", "child started", "parent finishing"]);
+  resolve();
+  return p.then(exitStatus => {
+    assert.equal(exitStatus, 42);
+    assert.logEquals(["parent started", "child started", "parent finishing", "child finishing"]);
+  });
+});
+
+test('spawn linked child microroutine', function(assert) {
+  return spawn(function * () {
+    yield spawnLinked(function * () {
+      assert.log('child ran');
+    });
+    assert.logEquals(['child ran']);
+  });
+});
+
+test('spawned task can enable error logging', function(assert) {
+  return spawn(function * () {
+    logErrors(err => {
+      assert.log('handled message: ' + err.message);
+    });
+    throw new Error('boom');
+  }).then(() => {
+    assert.ok(false, "should not get here");
+  }, err => {
+    assert.equal(err.message, 'boom');
+    assert.logEquals(['handled message: boom']);
+  });
+});
+
+test('error logging is inherited by linked children', function(assert) {
+  return spawn(function * () {
+    logErrors(err => {
+      assert.log('handled message: ' + err.message);
+    });
+    spawnLinked(function * () {
+      throw new Error('boom');
+    }).catch(() => {
+      assert.log('catching here too') // to avoid console noise
+    });
+  }).then(() => {
+    assert.logEquals(['handled message: boom', 'catching here too']);
+  });
+});
+
+test('propagating child error is logged once', function(assert) {
+  return spawn(function * () {
+    logErrors(err => {
+      assert.log('handled message: ' + err.message);
+    });
+    yield spawnLinked(function * () {
+      throw new Error('boom');
+    })
+  }).catch(err => {
+    assert.equal(err.message, 'boom');
+    assert.logEquals(['handled message: boom']);
+  });
+});
+
+test('spawnLinked requires a running microroutine', function(assert) {
+  assert.throws(() => {
+    spawnLinked(function * () {});
+  }, /spawnLinked: only works inside a running microroutine/);
+});
+
+['forward', 'reverse'].forEach(order => {
+  test(`resolves in ${order} order`, function(assert) {
+    let resolvers = [null, null];
+    let expected = ['hello', 'world'];
+    let promises = [null, null];
+
+    function * first() {
+      assert.log(yield new Promise(resolve => {
+        resolvers[0] = () => resolve('hello');
+      }));
+    }
+    function * second() {
+      assert.log(yield new Promise(resolve => {
+        resolvers[1] = () => resolve('world');
+      }));
+    }
+    return spawn(function * () {
+      promises[0] = spawn(first);
+      promises[1] = spawn(second);
+
+      if (order === 'reverse') {
+        resolvers = resolvers.reverse();
+        expected = expected.reverse();
+        promises = promises.reverse();
+      }
+
+      resolvers[0]();
+      yield promises[0];
+      resolvers[1]();
+      yield promises[1];
+      assert.logEquals(expected);
+    });
+  });
+});
+
+test('immediately returned promise', function(assert) {
+  return spawn(function * () {
+    let value = yield spawn(function * example() {
+      return new Promise(resolve => resolve(42));
+    });
+    assert.equal(value, 42);
+  });
+});
+
+test('asynchronously returned promise', function(assert) {
+  return spawn(function * () {
+    let resolveFirst;
+    let p = spawn(function * example() {
+      yield new Promise(r => resolveFirst = r);
+      return new Promise(resolve => resolve(42));
+    });
+    resolveFirst();
+    assert.equal(yield p, 42);
+  });
+});
+
+test('rejected promises trigger try/catch', function(assert) {
+  return spawn(function * () {
+    let reject;
+    let p = spawn(function * example() {
+      try {
+        yield new Promise((_, r) => reject = r);
+      } catch(err) {
+        assert.log(err);
+      }
+    });
+    reject('something');
+    yield p;
+    assert.logEquals(['something']);
+  });
+});
+
+test('can be stopped', function(assert) {
+  return spawn(function * () {
+    let task = spawn(function * () {
+      try {
+        let p = new Promise(() => null);
+        p.__ec_cancel__ = () => assert.log('cancelation ran');
+        yield p;
+      } finally {
+        assert.log('ran finally');
+      }
+    });
+    stop(task);
+    assert.logEquals(['cancelation ran', 'ran finally']);
+  });
+});
+
+test('can access self', function(assert) {
+  return spawn(function * () {
+    let innerTask;
+    let task = spawn(function * () {
+      innerTask = current();
+    });
+    assert.equal(innerTask, task);
+  });
+});
+
+test('can stop self', function(assert) {
+  return spawn(function * () {
+    spawn(function * () {
+      stop(current());
+      assert.ok(false, "should not get here");
+    });
+    assert.ok(true, "I get here");
+  });
+});
+
+
+test('can cancel all linked microroutines', function(assert) {
+  return spawn(function * () {
+    let resolveFirst, resolveSecond;
+
+    let task = spawn(function * () {
+
+      spawnLinked(function * example1() {
+        yield new Promise(r => resolveFirst = r);
+        let third = new Promise(() => null);
+        third.__ec_cancel__ = () => assert.log('third canceled');
+        try {
+          yield third;
+        } finally {
+          assert.log('third finally');
+        }
+      });
+
+      spawnLinked(function * example2() {
+        yield new Promise(r => resolveSecond = r);
+        let fourth = new Promise(() => null);
+        fourth.__ec_cancel__ = () => assert.log('fourth canceled');
+        try {
+          yield fourth;
+        } finally {
+          assert.log('fourth finally');
+        }
+      });
+
+    });
+
+    resolveFirst();
+    yield microwait();
+    resolveSecond();
+    yield microwait();
+    stop(task);
+    yield microwait();
+    assert.logContains('third canceled');
+    assert.logContains('fourth canceled');
+    assert.logContains('third finally');
+    assert.logContains('fourth finally');
+
+  });
+});
+
+test('can cancel all linked, from the inside', function(assert) {
+  return spawn(function * () {
+
+    let stopping;
+    let stopped = new Promise(r => stopping = r);
+
+    spawn(function * () {
+      let resolveFirst, resolveSecond;
+
+      spawnLinked(function * example1() {
+        yield new Promise(r => resolveFirst = r);
+        let third = new Promise(() => null);
+        third.__ec_cancel__ = () => assert.log('third canceled');
+        try {
+          yield third;
+        } finally {
+          assert.log('third finally');
+        }
+      });
+
+      spawnLinked(function * example2() {
+        yield new Promise(r => resolveSecond = r);
+        let fourth = new Promise(() => null);
+        fourth.__ec_cancel__ = () => assert.log('fourth canceled');
+        try {
+          yield fourth;
+        } finally {
+          assert.log('fourth finally');
+        }
+      });
+
+      resolveFirst();
+      yield microwait();
+      resolveSecond();
+      yield microwait();
+      stopping();
+      stop(current());
+
+    });
+
+    yield stopped;
+    yield microwait();
+    assert.logContains('third canceled');
+    assert.logContains('fourth canceled');
+    assert.logContains('third finally');
+    assert.logContains('fourth finally');
+
+  });
+});
+
+test('all linked microroutines are canceled if we die', function(assert) {
+  assert.expect(5);
+  return spawn(function * () {
+
+    let stopping;
+    let stopped = new Promise(r => stopping = r);
+
+    spawn(function * () {
+      let resolveFirst, resolveSecond;
+
+      spawnLinked(function * example1() {
+        yield new Promise(r => resolveFirst = r);
+        let third = new Promise(() => null);
+        third.__ec_cancel__ = () => assert.log('third canceled');
+        try {
+          yield third;
+        } finally {
+          assert.log('third finally');
+        }
+      });
+
+      spawnLinked(function * example2() {
+        yield new Promise(r => resolveSecond = r);
+        let fourth = new Promise(() => null);
+        fourth.__ec_cancel__ = () => assert.log('fourth canceled');
+        try {
+          yield fourth;
+        } finally {
+          assert.log('fourth finally');
+        }
+      });
+
+      resolveFirst();
+      yield microwait();
+      resolveSecond();
+      yield microwait();
+      stopping();
+      throw new Error('boom');
+    }).catch(err => {
+      assert.equal(err.message, 'boom');
+    });
+
+    yield stopped;
+    yield microwait();
+    assert.logContains('third canceled');
+    assert.logContains('fourth canceled');
+    assert.logContains('third finally');
+    assert.logContains('fourth finally');
+
+  });
+});
+
+test('fairness', function(assert) {
+  return spawn(function * () {
+
+    let a = spawnLinked(function * first() {
+      yield;
+      assert.log(1);
+      yield;
+      assert.log(2);
+      yield;
+      assert.log(3);
+    });
+
+    let b = spawnLinked(function * second() {
+      yield;
+      assert.log(4);
+      yield;
+      assert.log(5);
+      yield;
+      assert.log(6);
+    });
+
+    yield a;
+    yield b;
+
+  }).then(() => {
+    assert.logEquals([1, 4, 2, 5, 3, 6]);
+  });
+});
