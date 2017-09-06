81e5d73486af5daf91563fe2056d8769d7b372bb
Wang Zhongliang <zwang@rebelworks.io>
2017-09-06 23:51:20 +0800

    Update Hero demo to support Opacity transition

--- tests/dummy/app/controllers/hero/index.js
+++ tests/dummy/app/controllers/hero/index.js
@@ -1,14 +1,18 @@
 import Ember from 'ember';
 import Move from 'ember-animated/motions/move';
 import Scale from 'ember-animated/motions/scale';
+import Opacity from 'ember-animated/motions/opacity';
 
 export function * transition() {
   this.insertedSprites.forEach(sprite => {
-    sprite.reveal();
+    this.animate(new Opacity(sprite));
   });
 
   this.keptSprites.forEach(sprite => {
     sprite.scale(sprite.initialBounds.width / sprite.finalBounds.width, sprite.initialBounds.height / sprite.finalBounds.height);
+    sprite.applyStyles({
+      'z-index': 1
+    });
     this.animate(new Move(sprite));
     this.animate(new Scale(sprite));
   });
