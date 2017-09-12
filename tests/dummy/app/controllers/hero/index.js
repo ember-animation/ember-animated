57031f8c0484c6da957122f0894e5d2f48db41b1
Wang Zhongliang <zwang@rebelworks.io>
2017-09-12 15:29:14 +0800

    Revert "Reproduce the opacity transition bug"
    This reverts commit 9dcb94bf0769845519ea69d9fb94585c13d3d436.

--- tests/dummy/app/controllers/hero/index.js
+++ tests/dummy/app/controllers/hero/index.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 import Move from 'ember-animated/motions/move';
 import Scale from 'ember-animated/motions/scale';
-import Opacity from 'ember-animated/motions/opacity';
+import { FadeIn } from 'ember-animated/motions/opacity';
 
 export function * transition() {
+  this.insertedSprites.forEach(sprite => {
+    this.animate(new FadeIn(sprite));
+  });
+
   this.keptSprites.forEach(sprite => {
     sprite.scale(sprite.initialBounds.width / sprite.finalBounds.width, sprite.initialBounds.height / sprite.finalBounds.height);
     sprite.applyStyles({
@@ -11,7 +15,6 @@ export function * transition() {
     });
     this.animate(new Move(sprite));
     this.animate(new Scale(sprite));
-    this.animate(new Opacity(sprite));
   });
 }
 
