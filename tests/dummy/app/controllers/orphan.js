ab82caa830d882d77595c01a391e8907e9f1d048
Edward Faulkner <ef@alum.mit.edu>
2017-09-13 23:52:18 -0400

    improve orphan demo

--- tests/dummy/app/controllers/orphan.js
+++ tests/dummy/app/controllers/orphan.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 import Opacity from 'ember-animated/motions/opacity';
+import Move from 'ember-animated/motions/move';
 
 export default Ember.Controller.extend({
   showDetail: true,
-  transition,
+  fade,
+  fromSide,
   actions: {
     toggle() {
       this.set('showDetail', !this.get('showDetail'));
@@ -11,11 +13,23 @@ export default Ember.Controller.extend({
   }
 });
 
-function * transition() {
+function * fade() {
   this.insertedSprites.forEach(s => {
     this.animate(new Opacity(s, { from: 0 }));
   });
   this.removedSprites.forEach(s => {
     this.animate(new Opacity(s, { to: 0 }));
+  });
+}
+
+
+function * fromSide() {
+  this.insertedSprites.forEach(s => {
+    s.startAtPixel({ x: window.outerWidth * 0.8 });
+    this.animate(new Move(s));
+  });
+  this.removedSprites.forEach(s => {
+    s.endAtPixel({ x: window.outerWidth * 0.8 });
+    this.animate(new Move(s));
   });
 }
