4f5e505d4220575fe24dae0014437d2248b66f71
Edward Faulkner <ef@alum.mit.edu>
2017-09-15 16:52:08 -0400

    use native generators in development

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,9 +1,18 @@
 /* eslint-env node */
-module.exports = {
-  browsers: [
+
+let browsers;
+
+if (process.env.EMBER_ENV === 'development') {
+  browsers = [
+    'last 1 Chrome versions'
+  ];
+} else {
+  browsers = [
     'ie 9',
     'last 1 Chrome versions',
     'last 1 Firefox versions',
     'last 1 Safari versions'
   ]
-};
+}
+
+module.exports = { browsers };
