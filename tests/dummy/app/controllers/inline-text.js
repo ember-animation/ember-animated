abf05f8b28ec1c4b43eb39fc87b8cc63ac612dae
Edward Faulkner <ef@alum.mit.edu>
2017-09-15 12:16:56 -0400

    adding top-level animation to inline-text
    which currently prevents the individual animated-text components from animating at destruction.

--- tests/dummy/app/controllers/inline-text.js
+++ tests/dummy/app/controllers/inline-text.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
+import { FadeIn, FadeOut } from 'ember-animated/motions/opacity';
 
 export default Ember.Controller.extend({
   tableMode: false,
+  fade,
   actions: {
     toggle() {
       this.set('tableMode', !this.get('tableMode'));
     }
   }
 });
+
+function fade(initialRender) {
+  if (!initialRender) {
+    return function * () {
+      this.insertedSprites.forEach(s => this.animate(new FadeIn(s)));
+      this.removedSprites.forEach(s => this.animate(new FadeOut(s)));
+    };
+  }
+}
