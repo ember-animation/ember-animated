1a5100158bc5e410462638db5e059d151be094e9
Edward Faulkner <ef@alum.mit.edu>
2017-09-15 12:45:26 -0400

    adding failing tests for destruction early warning

--- tests/integration/components/animated-each-test.js
+++ tests/integration/components/animated-each-test.js
@@ -5,6 +5,8 @@ import $ from 'jquery';
 import { waitForAnimations } from 'ember-animated/test-helpers';
 import Ember from 'ember';
 const { A } = Ember;
+import { Promise } from 'ember-animated/concurrency-helpers';
+import Motion from 'ember-animated/motion';
 
 moduleForComponent('animated-each', 'Integration | Component | animated each', {
   integration: true,
@@ -243,4 +245,123 @@ test('it can match sprites that are leaving a destroyed component', function(ass
     return this.waitForAnimations();
   });
 
+});
+
+test('child animator can animate when a parent animator is planning to remove it', async function(assert) {
+  assert.expect(7);
+
+  function makeItem(id) {
+    return {
+      id,
+      comments: [{ id: `comment-${id}`}]
+    }
+  }
+
+  this.set('items', ['a', 'b', 'c'].map(makeItem));
+
+  this.render(hbs`
+    {{#animated-each items use=outerTransition key="id" as |item|}}
+      <div class="test-child">
+        {{item.id}}
+        {{#animated-each item.comments key="id" use=innerTransition as |comment|}}
+          <div>{{comment.id}}</div>
+        {{/animated-each}}
+      </div>
+    {{/animated-each}}
+  `);
+
+  await this.waitForAnimations();
+
+  this.set('outerTransition', function * () {
+    assert.deepEqual(this.keptSprites.map(s => s.owner.id), ['a', 'c'], 'kept sprites');
+    assert.deepEqual(this.insertedSprites.map(s => s.owner.id), [], 'inserted sprites');
+    assert.deepEqual(this.removedSprites.map(s => s.owner.id), ['b'], 'removed sprites');
+  });
+
+  let innerCounter = 0;
+
+  this.set('innerTransition', function * () {
+    innerCounter++;
+    assert.deepEqual(this.keptSprites.map(s => s.owner.id), [], 'kept sprites');
+    assert.deepEqual(this.insertedSprites.map(s => s.owner.id), [], 'inserted sprites');
+    assert.deepEqual(this.removedSprites.map(s => s.owner.id), ['comment-b'], 'removed sprites');
+  });
+
+  this.set('items', ['a', 'c'].map(makeItem));
+  await this.waitForAnimations();
+  assert.equal(innerCounter, 1, "inner transition should run once")
+});
+
+test('child animator reacts appropriately if its planned destruction is cancelled', async function(assert) {
+  assert.expect(7);
+
+  // a motion that never finishes
+  class TestMotion extends Motion {
+    *animate() {
+      yield new Promise(() => {});
+    }
+  }
+
+  function makeItem(id) {
+    return {
+      id,
+      comments: [{ id: `comment-${id}`}]
+    }
+  }
+
+  this.set('items', ['a', 'b', 'c'].map(makeItem));
+
+  this.render(hbs`
+    {{#animated-each items use=outerTransition key="id" as |item|}}
+      <div class="test-child">
+        {{item.id}}
+        {{#animated-each item.comments key="id" use=innerTransition as |comment|}}
+          <div>{{comment.id}}</div>
+        {{/animated-each}}
+      </div>
+    {{/animated-each}}
+  `);
+
+  await this.waitForAnimations();
+
+  // Create animations that will block forever and notify us when they have started
+  let sawOuter, sawInner;
+  let outerIsAnimating = new Promise(r => sawOuter = r);
+  let innerIsAnimating = new Promise(r => sawInner = r);
+  this.set('outerTransition', function * () {
+    this.removedSprites.forEach(s => this.animate(new TestMotion(s)));
+    sawOuter();
+  });
+  this.set('innerTransition', function * () {
+    this.removedSprites.forEach(s => this.animate(new TestMotion(s)));
+    sawInner();
+  });
+
+  // Trigger animations
+  this.set('items', ['a', 'c'].map(makeItem));
+
+  // Ensure that they have started
+  await outerIsAnimating;
+  await innerIsAnimating;
+
+  this.set('outerTransition', function * () {
+    assert.deepEqual(this.keptSprites.map(s => s.owner.id).sort(), ['a', 'b', 'c'], 'kept sprites');
+    assert.deepEqual(this.insertedSprites.map(s => s.owner.id), [], 'inserted sprites');
+    assert.deepEqual(this.removedSprites.map(s => s.owner.id), [], 'removed sprites');
+  });
+
+  let innerCounter = 0;
+  this.set('innerTransition', function * () {
+    innerCounter++;
+    assert.deepEqual(this.keptSprites.map(s => s.owner.id), ['comment-b'], 'kept sprites');
+    assert.deepEqual(this.insertedSprites.map(s => s.owner.id), [], 'inserted sprites');
+    assert.deepEqual(this.removedSprites.map(s => s.owner.id), [], 'removed sprites');
+  });
+
+
+  // Interrupt with a new animation that undoes the previous change
+  this.set('items', ['a', 'b', 'c'].map(makeItem));
+
+  await this.waitForAnimations();
+  assert.equal(innerCounter, 1, "inner transition should run once")
 });
