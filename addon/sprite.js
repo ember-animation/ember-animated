c3281916052708c98ab6827fa4c7a25a999efc64
Edward Faulkner <ef@alum.mit.edu>
2017-09-16 01:04:02 -0400

    update to match rebased master

--- addon/sprite.js
+++ addon/sprite.js
@@ -479,7 +479,7 @@ export default class Sprite {
   endAtSprite(otherSprite) {
     let diff = otherSprite.difference('finalBounds', this, 'initialBounds');
     this.endTranslatedBy(diff.dx, diff.dy);
-    this._finalBounds = resizedBounds(this.finalBounds, otherSprite.finalBounds.width, otherSprite.finalBounds.height);
+    this.finalBounds = resizedBounds(this.finalBounds, otherSprite.finalBounds.width, otherSprite.finalBounds.height);
     this.finalOpacity = otherSprite.finalOpacity;
   }
 
