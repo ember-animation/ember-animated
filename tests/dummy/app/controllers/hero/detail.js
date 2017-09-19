d23bc22b6561b26f03495ca110236b69130d6ec5
Edward Faulkner <ef@alum.mit.edu>
2017-09-19 13:55:10 -0400

    only received sprites need to be scaled to their starting dimensions

--- tests/dummy/app/controllers/hero/detail.js
+++ tests/dummy/app/controllers/hero/detail.js
@@ -3,7 +3,8 @@ import Move from 'ember-animated/motions/move';
 import Scale from 'ember-animated/motions/scale';
 
 function * transition() {
-  this.receivedSprites.concat(this.sentSprites).forEach(sprite => {
+
+  this.receivedSprites.forEach(sprite => {
     sprite.scale(sprite.initialBounds.width / sprite.finalBounds.width, sprite.initialBounds.height / sprite.finalBounds.height);
     sprite.applyStyles({
       'z-index': 1
@@ -11,8 +12,15 @@ function * transition() {
     this.animate(new Move(sprite));
     this.animate(new Scale(sprite));
   });
-}
 
+  this.sentSprites.forEach(sprite => {
+    sprite.applyStyles({
+      'z-index': 1
+    });
+    this.animate(new Move(sprite));
+    this.animate(new Scale(sprite));
+  });
+}
 
 export default Ember.Controller.extend({
   transition
