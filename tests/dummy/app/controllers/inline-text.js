f8243b0969709732b535d3cac35cdd5a69dfe2c4
Edward Faulkner <ef@alum.mit.edu>
2017-09-19 21:50:18 -0400

    make inline-text demo interruption-capable

--- tests/dummy/app/controllers/inline-text.js
+++ tests/dummy/app/controllers/inline-text.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import { FadeIn, FadeOut } from 'ember-animated/motions/opacity';
+import Opacity from 'ember-animated/motions/opacity';
 import { Promise } from 'ember-animated/concurrency-helpers';
 
 export default Ember.Controller.extend({
@@ -15,8 +15,25 @@ export default Ember.Controller.extend({
 function fade(initialRender) {
   if (!initialRender) {
     return function * () {
-      yield Promise.all(this.removedSprites.map(s => this.animate(new FadeOut(s, { duration: this.duration / 2}))));
-      this.insertedSprites.map(s => this.animate(new FadeIn(s, { duration: this.duration / 2 })));
+      // We yield Promise.all here because we want to wait for this
+      // step before starting what comes after.
+      yield Promise.all(this.removedSprites.map(s => {
+        if (s.revealed) {
+          return this.animate(new Opacity(s, {
+            to: 0,
+            duration: this.duration / 2
+          }));
+        }
+      }));
+
+      // Once all fading out has happened, we can fade in the inserted
+      // or kept sprites. Note that we get keptSprites if some things
+      // were fading out and then we get interrupted and decide to
+      // keep them around after all.
+      this.insertedSprites.concat(this.keptSprites).map(s => this.animate(new Opacity(s, {
+        to: 1,
+        duration: this.duration / 2
+      })));
     };
   }
 }
