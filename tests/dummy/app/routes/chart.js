acd7dc18a1a1b6f3f03d741b31d092202443ac83
Gareth Bosch <gbosch@gmail.com>
2018-01-31 22:27:35 -0900

    added method to concat the country and year key values in model

--- tests/dummy/app/routes/chart.js
+++ tests/dummy/app/routes/chart.js
@@ -6,53 +6,62 @@ import Papa from 'papaparse';
 export default Route.extend({
 
     async model() {
-        let pop = await loadcsv(`population.csv`);
-
         let model = [];
-        
-        // forEachColumn( pop, (country, year, population) =>
-        //     model.push({country, year, population})
-        // )
 
-        // let gdp = await loadcsv(`gdp_per_capita_ppp.csv`);
+        let pop = await loadcsv(`population.csv`);
+        forEachColumn( pop, (country, year, population) =>
+            model.push({country, year, population})
+        )
 
+        let gdp = await loadcsv(`gdp_per_capita_ppp.csv`);
+        forEachColumn(gdp, (country, year, gdp) => {
+            let datapoint = model.find(p => 
+                p.country === country && p.year === year);
+            if(datapoint) {
+                datapoint.gdp = gdp;
+            }
+        })
 
-        // forEachColumn(gdp, (country, year, gdp) => {
-        //     let datapoint = model.find(p => 
-        //         p.country === country && p.year === year);
-        //     if(datapoint) {
-        //         datapoint.gdp = gdp;
-        //     }
-        // })
+        let life = await loadcsv(`life_expectancy_at_birth.csv`);
+        forEachColumn(life, (country, year, life) => {
+            let datapoint = model.find(p => 
+                p.country === country && p.year === year);
+            if(datapoint) {
+                datapoint.life = life;
+            }
+        })
 
-        // let life = await loadcsv(`life_expectancy_at_birth.csv`);
+        concatCountryYear(model);
 
-        // forEachColumn(life, (country, year, life) => {
-        //     let datapoint = model.find(p => 
-        //         p.country === country && p.year === year);
-        //     if(datapoint) {
-        //         datapoint.life = life;
-        //     }
-        // })
+        /*  model array now is an array of objects. Each entry is structured thusly:
+            {
+                countryYear: "Japan 1988", 
+                population: "121432942", 
+                gdp: "26724", 
+                life: "78.54"
+            }
+        */
 
-        // model now is an array of objects. Each entry is structured thusly:
-        // {country: "Japan", year: "1988", population: "121432942", gdp: "26724", life: "78.54"}
-        
-        model = [
-            {country: "Albania", year: "2014", population: "2889676", gdp: "10160", life: "77.9"},
-            {country: "Cayman Islands", year: "1988", population: "22539", gdp: "52376"},
-            {country: "Georgia", year: "1989", population: "5466610", gdp: "9379", life: "69.95"}
-        ];
-        let newkey = model[0].country + model[0].year;
-        let newobj = { population: model[0].population};
-
-        return newkey;
+        return model;
       }
-
 });
 
+/* Concatonates the 'country' and 'year' key values into one key.
+   The new key is 'countryYear'. Each entry in the model array
+   will have this new key in replace of the 'country' and 
+   'year' keys.
+*/
+function concatCountryYear(model) {
+    model.forEach(row => {
+        let newKey = row.country + " " + row.year;
+        delete row.country;
+        delete row.year;
+        row.countryYear = newKey;
+    })
+}
+
 async function loadcsv(url) {
-    let response = await fetch(url)
+    let response = await fetch(url);
     let text = await response.text();
     return Papa.parse(text, {header: true}).data;
 }
@@ -61,28 +70,9 @@ function forEachColumn(rows, body) {
     rows.forEach(row => {
         Object.keys(row).forEach(key => {
             if(row[key] && key !== 'country'){
-                body(row.country, key, row[key])
+                body(row.country, key, row[key]);
             }
         })
     })
 }
 
-// function concatCountryYear(model) {
-//     model.forEach(row => {
-        
-//     })
-// }
-
-// Object.keys(row).forEach(key => {
-            
-// })
-
-
-// o[ new_key ] = o[ old_key ];
-// delete o[ old_key ];
-
-// if (old_key !== new_key) {
-//     Object.defineProperty(o, new_key,
-//         Object.getOwnPropertyDescriptor(o, old_key));
-//     delete o[old_key];
-// }
