e60a9d666a295bc24fdc7c5aca71d2d81eb31b00
Gareth Bosch <gbosch@gmail.com>
2018-02-06 22:22:32 -0900

    transformed x-axis (income) to a log base 2 scale

--- tests/dummy/app/helpers/scale-y.js
+++ tests/dummy/app/helpers/scale-y.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function scaleY([value], {minValue, maxValue}) {
-  return `${100 - (100*(value - minValue)/(maxValue - minValue))}%`;
+  return `${100 - (100 * (value - minValue)/(maxValue - minValue))}%`;
 
 }
 
--- tests/dummy/app/helpers/scale.js
+++ tests/dummy/app/helpers/scale.js
@@ -1,7 +1,10 @@
 import { helper } from '@ember/component/helper';
 
 export function scale([value], {minValue, maxValue}) {
-  return `${100*(value - minValue)/(maxValue - minValue)}%`;
+  value = Math.log2(value);
+  minValue = Math.log2(minValue);
+  maxValue = Math.log2(maxValue);
+  return `${ 100 * (value - minValue) / (maxValue - minValue) }%`;
 
 }
 export default helper(scale);
