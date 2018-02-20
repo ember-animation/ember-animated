f88b625a2d40e70fec7718e892697dd56e16176d
Edward Faulkner <ef@alum.mit.edu>
2018-02-20 09:22:11 -0500

    move motion-indicator and time-control into a separate addon

--- tests/dummy/app/components/motion-indicator.js
+++
@@ -1,8 +0,0 @@
-import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-
-export default Component.extend({
-  classNames: ['motion-indicator'],
-  classNameBindings: ['motionService.isAnimating:active'],
-  motionService: service('-ea-motion')
-});
