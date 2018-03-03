d39c76ec55872358a42d3f2f91c7fcbd128024c0
Edward Faulkner <ef@alum.mit.edu>
2018-03-02 22:39:46 -0500

    fixing passthrough of initialInsertion
    and improving test coverage on orphans route

--- tests/acceptance/orphan-test.js
+++ tests/acceptance/orphan-test.js
@@ -1,5 +1,5 @@
 import { test, module } from 'qunit';
-import { TimeControl } from 'ember-animated/test-support';
+import { TimeControl, animationsSettled } from 'ember-animated/test-support';
 import { setupApplicationTest } from 'ember-qunit';
 import { currentURL, visit } from '@ember/test-helpers';
 
@@ -19,7 +19,18 @@ module('Acceptance | orphan', function(hooks) {
   });
 
   test('visiting /orphan', async function(assert) {
+    time.pause();
     await visit('/orphan');
+    await time.advance(1000);
+
+    let opacity = parseFloat(getComputedStyle(this.element.querySelector('.one')).opacity);
+    assert.ok(opacity > 0 && opacity < 1, `expected opacity to be animating, it's ${opacity}`);
+
+    let onePosition = this.element.querySelector('.one').getBoundingClientRect().left;
+    let twoPosition = this.element.querySelector('.two').getBoundingClientRect().left;
+    assert.ok(twoPosition > onePosition, `expected element .two to be animating in, ${twoPosition } > ${onePosition}`);
+    time.runAtSpeed(40);
+    await animationsSettled();
     assert.equal(currentURL(), '/orphan');
   });
 });
--- tests/dummy/app/controllers/orphan.js
+++ tests/dummy/app/controllers/orphan.js
@@ -4,30 +4,28 @@ import move from 'ember-animated/motions/move';
 
 export default Controller.extend({
   showDetail: true,
-  fade,
-  fromSide,
+
+  fade: function*({ insertedSprites, receivedSprites, removedSprites }) {
+    insertedSprites.forEach(s => opacity(s, { from: 0 }));
+    receivedSprites.forEach(s => opacity(s));
+    removedSprites.forEach(s => opacity(s, { to: 0 }));
+  },
+
+  fromSide: function*({ insertedSprites, receivedSprites, removedSprites }) {
+    insertedSprites.forEach(s => {
+      s.startAtPixel({ x: window.outerWidth * 0.8 });
+      move(s);
+    });
+    receivedSprites.forEach(move);
+    removedSprites.forEach(s => {
+      s.endAtPixel({ x: window.outerWidth * 0.8 });
+      move(s);
+    });
+  },
+
   actions: {
     toggle() {
       this.set('showDetail', !this.get('showDetail'));
     }
   }
 });
-
-function * fade({ insertedSprites, receivedSprites, removedSprites }) {
-  insertedSprites.forEach(s => opacity(s, { from: 0 }));
-  receivedSprites.forEach(s => opacity(s));
-  removedSprites.forEach(s => opacity(s, { to: 0 }));
-}
-
-
-function * fromSide({ insertedSprites, receivedSprites, removedSprites }) {
-  insertedSprites.forEach(s => {
-    s.startAtPixel({ x: window.outerWidth * 0.8 });
-    move(s);
-  });
-  receivedSprites.forEach(move);
-  removedSprites.forEach(s => {
-    s.endAtPixel({ x: window.outerWidth * 0.8 });
-    move(s);
-  });
-}
