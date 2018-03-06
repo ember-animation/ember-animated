8b37fffb3109eb8b4c17b8db7eacbafb51e7a1f7
Edward Faulkner <ef@alum.mit.edu>
2018-03-06 00:40:33 -0500

    adding postcss and cleaning up peer dep warnings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,21 @@
 'use strict';
 
+const CssImport = require('postcss-import');
+const CssNext = require('postcss-cssnext');
+
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    postcssOptions: {
+      compile: {
+        enabled: true,
+        plugins: [
+          { module: CssImport },
+          { module: CssNext }
+        ]
+      }
+    }
   });
   return app.toTree();
 };
