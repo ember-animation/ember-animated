693c532d953b1cadc9f520d7066f3d4996930ffd
Edward Faulkner <ef@alum.mit.edu>
2018-03-07 20:26:50 -0500

    make fade available as a reusable transition

---
+++ addon/transitions/fade.js
@@ -0,0 +1,24 @@
+import opacity from 'ember-animated/motions/opacity';
+import { Promise } from 'ember-animated';
+
+export default function * ({ removedSprites, insertedSprites, keptSprites, duration }) {
+  // We yield Promise.all here because we want to wait for this
+  // step before starting what comes after.
+  yield Promise.all(removedSprites.map(s => {
+    if (s.revealed) {
+      return opacity(s, {
+        to: 0,
+        duration: duration / 2
+      });
+    }
+  }));
+
+  // Once all fading out has happened, we can fade in the inserted
+  // or kept sprites. Note that we get keptSprites if some things
+  // were fading out and then we get interrupted and decide to
+  // keep them around after all.
+  insertedSprites.concat(keptSprites).map(s => opacity(s, {
+    to: 1,
+    duration: duration / 2
+  }));
+}
--- tests/dummy/app/controllers/inline-text.js
+++ tests/dummy/app/controllers/inline-text.js
@@ -1,30 +1,9 @@
 import Controller from '@ember/controller';
-import opacity from 'ember-animated/motions/opacity';
-import { Promise } from 'ember-animated';
+import fade from 'ember-animated/transitions/fade';
 
 export default Controller.extend({
   tableMode: false,
-  fade: function * ({ removedSprites, insertedSprites, keptSprites, duration }) {
-    // We yield Promise.all here because we want to wait for this
-    // step before starting what comes after.
-    yield Promise.all(removedSprites.map(s => {
-      if (s.revealed) {
-        return opacity(s, {
-          to: 0,
-          duration: duration / 2
-        });
-      }
-    }));
-
-    // Once all fading out has happened, we can fade in the inserted
-    // or kept sprites. Note that we get keptSprites if some things
-    // were fading out and then we get interrupted and decide to
-    // keep them around after all.
-    insertedSprites.concat(keptSprites).map(s => opacity(s, {
-      to: 1,
-      duration: duration / 2
-    }));
-  },
+  fade,
 
   actions: {
     toggle() {
