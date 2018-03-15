16eb55b1a5547ff5c0e15da82bc47de19768ef1b
Edward Faulkner <ef@alum.mit.edu>
2018-03-15 14:43:44 -0700

    use fixed addon-docs that doesn't alter our adapters

--- tests/dummy/app/adapters/person.js
+++ tests/dummy/app/adapters/person.js
@@ -14,7 +14,7 @@ export default DS.JSONAPIAdapter.extend({
           id: String(i),
           attributes: Object.seal({
             name: faker.name.firstName(),
-            avatarUrl: faker.internet.avatar()
+            'avatar-url': faker.internet.avatar()
           })
         }));
       }
