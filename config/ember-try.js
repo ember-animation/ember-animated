155c37d02f408a4c4a7a788dd25f4cd507f49af2
Edward Faulkner <ef@alum.mit.edu>
2018-03-16 08:56:30 -0700

    switching to yarn in ember-try
    To avoid possible npm5 issues

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,6 +9,8 @@ module.exports = function() {
     getChannelURL('canary'),
   ]).then((urls) => {
     return {
+      useYarn: true,
+
       scenarios: [
         {
           name: 'ember-lts-2.12',
