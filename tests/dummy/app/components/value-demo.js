03b89e151f8177ed8aa9424570e16c913e9b83b6
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-07-09 11:07:42 -0400

    cleanup

--- tests/dummy/app/components/value-demo.js
+++ tests/dummy/app/components/value-demo.js
@@ -1,7 +1,4 @@
-/* if changes are made here please also reflect them in
-the comments in the docs */
 
-//BEGIN-SNIPPET value-snippet.js
 import Component from '@ember/component';
 import { toLeft, toRight } from 'ember-animated/transitions/move-over';
 
@@ -25,4 +22,4 @@ export default Component.extend({
     }
   }
 });
-//END-SNIPPET
+
