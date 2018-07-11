a30b1d0d4dda640f561bfd63d2e276f16f61fb2e
Edward Faulkner <ef@alum.mit.edu>
2018-07-10 20:24:56 -0400

    Returning to dynamic ember-release scenario
    The upstream release data at https://s3.amazonaws.com/builds.emberjs.com/release.json got fixed.

--- config/ember-try.js
+++ config/ember-try.js
@@ -43,7 +43,7 @@ module.exports = function() {
           name: 'ember-release',
           npm: {
             devDependencies: {
-              'ember-source': '~3.2.2'
+              'ember-source': urls[0]
             }
           }
         },
