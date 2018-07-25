0d392228beaa5d74540c9e64e6b63d8d3d95cf4b
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-07-25 12:23:13 -0400

    changed tableMode to listMode

--- tests/dummy/app/components/moving-word-text.js
+++ tests/dummy/app/components/moving-word-text.js
@@ -3,12 +3,12 @@ import Component from '@ember/component';
 import fade from 'ember-animated/transitions/fade';
 
 export default Component.extend({
-  tableMode: false,
+  listMode: false,
   fade,
 
   actions: {
     toggle() {
-      this.set('tableMode', !this.get('tableMode'));
+      this.set('listMode', !this.get('listMode'));
     },
     normal(){
       this.set('noAdjustments', !this.get('noAdjustments'));
