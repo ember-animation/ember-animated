7a1b2d94dfa1cabfd1caab53cd8e809a2eae1643
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-07-27 15:10:14 -0400

    full sprite log with all sprite categories

---
+++ tests/dummy/app/components/full-log-table.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import { A } from '@ember/array';
+
+function printSprites (context) {
+  return {
+    inserted: context._insertedSprites.map(s =>  s.owner.value.message),
+    kept: context._keptSprites.map(s => s.owner.value.message),
+    removed: context._removedSprites.map(s => s.owner.value.message),
+    sent: context._sentSprites.map(s => s.owner.value.message),
+    received: context._receivedSprites.map(s => s.owner.value.message)
+  };
+}
+
+export default Component.extend({
+  tagName: '',
+  init() {
+    this._super();
+    this.messages = A();
+  },
+  fullLog(context) {
+    this.messages.pushObject(printSprites(context));
+  }
+});
+
+export const extensions = {
+  init() {
+    this._super();
+    this.transition = this.transition.bind(this);
+  },
+  transition: function(context) {
+    this.fullLog(context);
+    return this._super(context);
+  }
+};
