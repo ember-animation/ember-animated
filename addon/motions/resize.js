4a98aa2623dc9fe860f7fe67a70bfa50dd08129b
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-28 15:33:29 -0400

    Update Opacity, Resize motions to respect custom easing

--- addon/motions/resize.js
+++ addon/motions/resize.js
@@ -1,4 +1,4 @@
-import { Motion, rAF, Tween } from '..';
+import { Motion, rAF, Tween } from "..";
 
 /**
   Smoothly resizes _sprite_ from its the initial size to its final size.
@@ -35,35 +35,43 @@ export class Resize extends Motion {
     this.prior = motions.find(m => m instanceof this.constructor);
   }
 
-  * animate() {
+  *animate() {
     let sprite = this.sprite;
     let duration = this.duration;
 
     if (!this.prior) {
       this.widthTween = new Tween(
         sprite.initialBounds.width / sprite.initialCumulativeTransform.a,
-        sprite.finalBounds.width / sprite.finalCumulativeTransform.a, duration
+        sprite.finalBounds.width / sprite.finalCumulativeTransform.a,
+        duration,
+        this.opts.easing
       );
       this.heightTween = new Tween(
         sprite.initialBounds.height / sprite.initialCumulativeTransform.d,
-        sprite.finalBounds.height / sprite.finalCumulativeTransform.d, duration
+        sprite.finalBounds.height / sprite.finalCumulativeTransform.d,
+        duration,
+        this.opts.easing
       );
     } else {
       this.widthTween = new Tween(
         0,
-        sprite.finalBounds.width / sprite.finalCumulativeTransform.a - this.prior.sprite.finalBounds.width,
-        duration
+        sprite.finalBounds.width / sprite.finalCumulativeTransform.a -
+          this.prior.sprite.finalBounds.width,
+        duration,
+        this.opts.easing
       ).plus(this.prior.widthTween);
       this.heightTween = new Tween(
         0,
-        sprite.finalBounds.height / sprite.finalCumulativeTransform.d - this.prior.sprite.finalBounds.height,
-        duration
+        sprite.finalBounds.height / sprite.finalCumulativeTransform.d -
+          this.prior.sprite.finalBounds.height,
+        duration,
+        this.opts.easing
       ).plus(this.prior.heightTween);
     }
 
     while (!this.widthTween.done || !this.heightTween.done) {
       sprite.applyStyles({
-        width:this.widthTween.currentValue,
+        width: this.widthTween.currentValue,
         height: this.heightTween.currentValue
       });
       yield rAF();
--- addon/motions/scale.js
+++ addon/motions/scale.js
@@ -1,4 +1,4 @@
-import { Motion, rAF, Tween } from '..';
+import { Motion, rAF, Tween } from "..";
 
 /**
   Smoothly scales _sprite_ from its the initial size to its final size.
@@ -28,7 +28,7 @@ export class Scale extends Motion {
     this.heightTween = null;
   }
 
-  * animate() {
+  *animate() {
     let sprite = this.sprite;
     let duration = this.duration;
 
@@ -39,22 +39,39 @@ export class Scale extends Motion {
       // which may differ from the initialBounds.width &
       // initialBounds.height that have been set for it. This
       // compensates with an initial scaling.
-      initialWidthFactor =  sprite.initialBounds.width / sprite.originalInitialBounds.width;
-      initialHeightFactor =  sprite.initialBounds.height / sprite.originalInitialBounds.height;
-    } else  {
+      initialWidthFactor =
+        sprite.initialBounds.width / sprite.originalInitialBounds.width;
+      initialHeightFactor =
+        sprite.initialBounds.height / sprite.originalInitialBounds.height;
+    } else {
       // the sprite is going to start at its own native final size
-      initialWidthFactor =  sprite.initialBounds.width / sprite.originalFinalBounds.width;
-      initialHeightFactor =  sprite.initialBounds.height / sprite.originalFinalBounds.height;
+      initialWidthFactor =
+        sprite.initialBounds.width / sprite.originalFinalBounds.width;
+      initialHeightFactor =
+        sprite.initialBounds.height / sprite.originalFinalBounds.height;
     }
 
     let widthFactor = sprite.finalBounds.width / sprite.initialBounds.width;
     let heightFactor = sprite.finalBounds.height / sprite.initialBounds.height;
 
-    this.widthTween = new Tween(sprite.transform.a * initialWidthFactor, sprite.transform.a * initialWidthFactor * widthFactor, duration, this.opts.easing);
-    this.heightTween = new Tween(sprite.transform.d * initialHeightFactor, sprite.transform.d * initialHeightFactor * heightFactor, duration, this.opts.easing);
+    this.widthTween = new Tween(
+      sprite.transform.a * initialWidthFactor,
+      sprite.transform.a * initialWidthFactor * widthFactor,
+      duration,
+      this.opts.easing
+    );
+    this.heightTween = new Tween(
+      sprite.transform.d * initialHeightFactor,
+      sprite.transform.d * initialHeightFactor * heightFactor,
+      duration,
+      this.opts.easing
+    );
 
     while (!this.widthTween.done || !this.heightTween.done) {
-      sprite.scale(this.widthTween.currentValue / sprite.transform.a, this.heightTween.currentValue / sprite.transform.d);
+      sprite.scale(
+        this.widthTween.currentValue / sprite.transform.a,
+        this.heightTween.currentValue / sprite.transform.d
+      );
       yield rAF();
     }
   }
