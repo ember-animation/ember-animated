c76849ce4cf5703a16092a3596693aac39a89f36
Edward Faulkner <ef@alum.mit.edu>
2019-10-09 17:20:07 -0400

    cleaning up dependencies in addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,19 +5,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     vendorFiles: { 'jquery.js': null, 'app-shims.js': null },
-    babel: {
-      plugins: [
-        'transform-class-properties',
-      ]
-    },
-    'ember-cli-uglify': {
-			uglify: {
-				compress: {
-          // https://github.com/ember-cli/ember-cli/issues/8075
-					collapse_vars: false
-				}
-			}
-		},
   });
   return app.toTree();
 };
