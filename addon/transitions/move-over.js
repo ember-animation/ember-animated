8e2a61c39ae038d338d4c4823ad4cac0da97f73f
Dustin Wilhelmi <dustin.wilhelmi@olo.com>
2020-08-07 11:09:59 -0500

    Support receivedSprites in moveOver

--- addon/transitions/move-over.js
+++ addon/transitions/move-over.js
@@ -56,6 +56,8 @@ export default function* moveOver(dimension, direction, context) {
     viewport = context.insertedSprites[0].finalBounds;
   } else if (context.keptSprites.length) {
     viewport = context.keptSprites[0].finalBounds;
+  } else if (context.receivedSprites.length) {
+    viewport = context.receivedSprites[0].finalBounds;
   } else {
     throw new Error('Unimplemented');
   }
@@ -94,6 +96,12 @@ export default function* moveOver(dimension, direction, context) {
     }
   } else if (context.keptSprites.length) {
     let move = new Move(context.keptSprites[0]);
+    move.run();
+    context.removedSprites.forEach(sprite => {
+      follow(sprite, { source: move });
+    });
+  } else if (context.receivedSprites.length) {
+    let move = new Move(context.receivedSprites[0]);
     move.run();
     context.removedSprites.forEach(sprite => {
       follow(sprite, { source: move });
