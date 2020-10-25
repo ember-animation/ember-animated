942f90df2aa36c7d6a7759f33cc4504359447efe
Sergey Astapov <sergeastapov@gmail.com>
2020-10-24 22:27:32 -0400

    Add 3.20 LTS to tests matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -31,6 +31,14 @@ module.exports = async function() {
         },
       },
       {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.5',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
