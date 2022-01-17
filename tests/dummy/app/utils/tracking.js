788e6bdd320f11cbb86129b9096969762fc097e0
Sergey Astapov <sergeastapov@gmail.com>
2022-01-17 00:27:53 -0500

    Fix dependencySatisfies usage for ember-source version check

--- tests/dummy/app/utils/tracking.js
+++ tests/dummy/app/utils/tracking.js
@@ -1,6 +1,10 @@
 import { tracked as nativeTracked } from '@glimmer/tracking';
 import { dependentKeyCompat as nativeDependentKeyCompat } from '@ember/object/compat';
-import { dependencySatisfies, importSync } from '@embroider/macros';
+import {
+  dependencySatisfies,
+  macroCondition,
+  importSync,
+} from '@embroider/macros';
 
 let tracked;
 let dependentKeyCompat;
@@ -13,15 +17,16 @@ let dependentKeyCompat;
  * once we drop support for Ember versions older than 3.13 in CI.
  */
 if (
-  dependencySatisfies('ember-source', '<= 3.12') &&
-  dependencySatisfies('ember-tracked-polyfill', '*')
+  macroCondition(
+    dependencySatisfies('ember-source', '>=3.13.0-canary || >=3.13.0-beta'),
+  )
 ) {
+  tracked = nativeTracked;
+  dependentKeyCompat = nativeDependentKeyCompat;
+} else {
   let polyfill = importSync('ember-tracked-polyfill');
   tracked = polyfill.tracked;
   dependentKeyCompat = polyfill.dependentKeyCompat;
-} else {
-  tracked = nativeTracked;
-  dependentKeyCompat = nativeDependentKeyCompat;
 }
 
 export { tracked, dependentKeyCompat };
