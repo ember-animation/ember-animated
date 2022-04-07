1b1678a2c34c733109f936f8475d307077c8a35d
Sergey Astapov <SergeAstapov@gmail.com>
2022-04-07 14:12:52 -0400

    Remove `treePaths.addon` override

--- addon/addon-main.js
+++ addon/addon-main.js
@@ -1,9 +1,2 @@
 const { addonV1Shim } = require('@embroider/addon-shim');
-module.exports = {
-  ...addonV1Shim(__dirname),
-  init(...args) {
-    this._super.init.apply(this, args);
-
-    this.treePaths.addon = 'src';
-  },
-};
+module.exports = addonV1Shim(__dirname);
